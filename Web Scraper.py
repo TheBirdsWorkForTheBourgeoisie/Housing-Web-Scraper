aacec39aa4c012361775fd264fcf035c10ec627f
TheBirdsWorkForTheBourgeoisie <80783226+TheBirdsWorkForTheBourgeoisie@users.noreply.github.com>
2022-03-28 16:54:48 -0600

    Add files via upload

---
+++ Web Scraper.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+soup = BeautifulSoup(html_text, 'lxml')
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+for job in jobs:
+	published_date = job.find('span', class_='sim-posted').span.text
+	if 'few' in published_date:
+
+		companny_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','')
+		skills = job.find('span', class_='srp-skills').text.replace(' ','')
+		more_info = job.header.h2.a['href']
+
+		print(f"Companny Name: {companny_name.strip()}")
+		print(f"Required Skills: {skills.strip()}")
+		print(f"More more_info: {more_info.strip()}")
+
+
+		print('')
+
